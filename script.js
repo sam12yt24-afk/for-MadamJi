63ad11b868ed8bad977b21342ad1ed01cdc75430
Sam <sam12yt24@gmail.com>
2026-08-03 16:35:14 +0530

    bleh

--- script.js
+++ script.js
@@ -32,6 +32,9 @@ const starContainer = document.getElementById("star-container");
 const shootingContainer =
 document.querySelector(".shooting-stars");
 
+const particleContainer =
+document.getElementById("particleContainer");
+
 
 
 // ======================================================
@@ -152,10 +155,48 @@ starContainer.appendChild(star);
 
 
 // ======================================================
+// PARTICLE GENERATOR
+// ======================================================
+
+function createParticles(total=35){
+
+for(let i=0;i<total;i++){
+
+const particle=document.createElement("div");
+
+particle.className="particle";
+
+const size=Math.random()*2+1;
+
+particle.style.width=size+"px";
+
+particle.style.height=size+"px";
+
+particle.style.left=Math.random()*100+"%";
+
+particle.style.top=Math.random()*100+"%";
+
+particle.style.animationDuration=
+(Math.random()*12+14)+"s";
+
+particle.style.animationDelay=
+(Math.random()*14)+"s";
+
+particleContainer.appendChild(particle);
+
+}
+
+}
+
+
+
+// ======================================================
 // INITIALIZE BACKGROUND
 // ======================================================
 
 createStars();
+
+createParticles();
 // ======================================================
 // PART 1B
 // SHOOTING STARS + TYPEWRITER ENGINE
